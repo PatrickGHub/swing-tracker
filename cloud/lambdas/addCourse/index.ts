06ae66d84baa37defa2b437df092e18d7e347508
PatrickGHub <patrick@pkelly.co>
2022-08-19 00:03:30 +0100

    Delete DB items through lambda

--- cloud/lambdas/addCourse/index.ts
+++ cloud/lambdas/addCourse/index.ts
@@ -11,17 +11,36 @@ export const handler = async (event: APIGatewayProxyEventV2) => {
   let body: string
 
   try {
-    await dynamo.put({
-      TableName: 'Courses',
-      Item: {
-        Name: eventBody.name,
-        Holes: eventBody.holes,
-        Par: eventBody.par
-      }
-    })
-    .promise()
+    switch(eventBody.action) {
+      case 'PUT':
+        await dynamo.put({
+          TableName: 'Courses',
+          Item: {
+            Name: eventBody.name,
+            Holes: eventBody.holes,
+            Par: eventBody.par
+          }
+        })
+        .promise()
+    
+        body = `Added course "${eventBody.name}" to database`
+        break
 
-    body = `Added course: ${eventBody.name} to database`
+      case 'DELETE':
+        await dynamo.delete({
+          TableName: 'Courses',
+          Key: {
+            Name: eventBody.name
+          }
+        })
+        .promise()
+    
+        body = `Deleted course "${eventBody.name}" from database`
+        break
+
+      default:
+        throw new Error(`Invalid action of ${eventBody.action} provided`)
+    }
   } catch (error) {
     statusCode = 400
     body = error.message

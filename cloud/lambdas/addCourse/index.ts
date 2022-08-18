c260cdeab4a9da4a7ce257278958447b351eb30e
PatrickGHub <patrick@pkelly.co>
2022-08-18 23:12:44 +0100

    Save course to DB through lambda

--- cloud/lambdas/addCourse/index.ts
+++ cloud/lambdas/addCourse/index.ts
@@ -1,8 +1,30 @@
-export const handler = () => {
-  console.log('Lambda running')
+const AWS = require('aws-sdk')
+
+const dynamo = new AWS.DynamoDB.DocumentClient()
+
+export const handler = async (event) => {
+  let statusCode: number = 200
+  let body: string
+
+  try {
+    await dynamo.put({
+      TableName: 'Courses',
+      Item: {
+        Name: event.name,
+        Holes: event.holes,
+        Par: event.par
+      }
+    })
+    .promise()
+
+    body = `Added course: ${event.name} to database`
+  } catch (error) {
+    statusCode = 400
+    body = error.message
+  }
 
   return {
-    statusCode: 200,
-    body: 'Returning 200'
+    statusCode,
+    body
   }
 }

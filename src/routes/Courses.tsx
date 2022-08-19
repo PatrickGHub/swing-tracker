795ec942b22d4c095ee65603dffda68bbec22529
PatrickGHub <patrick@pkelly.co>
2022-08-19 10:59:12 +0100

    Get course data through API gateway

--- src/routes/Courses.tsx
+++ src/routes/Courses.tsx
@@ -1,4 +1,5 @@
-import { SyntheticEvent, useState } from 'react'
+import { SyntheticEvent, useEffect, useState } from 'react'
+import axios from 'axios'
 import { ICourseData } from '../ts/interfaces'
 import CourseCard from '../components/CourseCard'
 import HolesCard from '../components/HolesCard'
@@ -6,125 +7,33 @@ import '../scss/courses.scss'
 
 const Courses = () => {
   const [selectedCourse, setSelectedCourse] = useState<ICourseData | null>()
+  const [courses, setCourses] = useState<ICourseData[]>([])
 
   const handleCourseSelect = (event: SyntheticEvent<HTMLButtonElement>) => {
     const clickedCourseName = event.currentTarget.getAttribute('data-coursename')
-    setSelectedCourse(coursesData.find((course) => course.name === clickedCourseName))
+    setSelectedCourse(courses.find((course) => course.name === clickedCourseName))
   }
 
-  const coursesData = [
-    {
-      name: 'Course 1',
-      holes: 9,
-      par: 27,
-      yards: 1100,
-      holesData: [
-        {
-          hole: 1,
-          yards: 101,
-          par: 3
-        },
-        {
-          hole: 2,
-          yards: 102,
-          par: 3
-        },
-        {
-          hole: 3,
-          yards: 103,
-          par: 3
-        },
-        {
-          hole: 4,
-          yards: 104,
-          par: 3
-        },
-        {
-          hole: 5,
-          yards: 105,
-          par: 3
-        },
-        {
-          hole: 6,
-          yards: 106,
-          par: 3
-        },
-        {
-          hole: 7,
-          yards: 107,
-          par: 3
-        },
-        {
-          hole: 8,
-          yards: 108,
-          par: 3
-        },
-        {
-          hole: 9,
-          yards: 109,
-          par: 3
-        },
-      ]
-    },
-    {
-      name: 'Course 2',
-      holes: 9,
-      par: 27,
-      yards: 1200,
-      holesData: [
-        {
-          hole: 1,
-          yards: 101,
-          par: 3
-        },
-        {
-          hole: 2,
-          yards: 102,
-          par: 3
-        },
-        {
-          hole: 3,
-          yards: 103,
-          par: 3
-        },
-        {
-          hole: 4,
-          yards: 104,
-          par: 3
-        },
-        {
-          hole: 5,
-          yards: 105,
-          par: 3
-        },
-        {
-          hole: 6,
-          yards: 106,
-          par: 3
-        },
-        {
-          hole: 7,
-          yards: 107,
-          par: 3
-        },
-        {
-          hole: 8,
-          yards: 108,
-          par: 3
-        },
-        {
-          hole: 9,
-          yards: 109,
-          par: 3
-        },
-      ]
+  useEffect(() => {
+    const getAllCourses = async () => {
+      const allCourses = await axios({
+        method: 'POST',
+        url: `${process.env.REACT_APP_API_GATEWAY}/courses`,
+        data: {
+          action: 'GET_ALL'
+        }
+      })
+
+      setCourses(allCourses.data.Items)
     }
-  ]
+
+    getAllCourses()
+  }, [])
 
   return (
     <div className='coursesRoute'>
       <div>
-        {coursesData.map((course: ICourseData) => (
+        {courses && courses.map((course: ICourseData) => (
           <CourseCard
             key={course.name}
             course={course}

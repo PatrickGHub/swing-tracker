084241cb887ef297617719431686e3eec61c7c15
PatrickGHub <patrick@pkelly.co>
2022-08-21 14:31:02 +0100

    Add rounds table

--- cloud/dynamoDB.tf
+++ cloud/dynamoDB.tf
@@ -10,3 +10,16 @@ resource "aws_dynamodb_table" "courses_table" {
     type = "S"
   }
 }
+
+resource "aws_dynamodb_table" "rounds_table" {
+  name = "Rounds"
+  billing_mode = "PROVISIONED"
+  hash_key = "id"
+  read_capacity = 1
+  write_capacity = 1
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}

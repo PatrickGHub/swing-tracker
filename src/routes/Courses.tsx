b07416351c70f1dc8e4775b59c98e8e52e2ca9ad
PatrickGHub <patrick@pkelly.co>
2023-01-03 16:44:00 +1100

    Get course data from generic data route

--- src/routes/Courses.tsx
+++ src/routes/Courses.tsx
@@ -27,135 +27,136 @@ const Courses = () => {
     setSelectedCourse(null)
   }
 
-  // useEffect(() => {
-  //   const getAllCourses = async () => {
-  //     const allCourses = await axios({
-  //       method: 'POST',
-  //       url: `${process.env.REACT_APP_API_GATEWAY}/courses`,
-  //       headers: {
-  //         'x-api-key': `${process.env.REACT_APP_COURSES_API_KEY}`
-  //       },
-  //       data: {
-  //         action: 'GET_ALL'
-  //       }
-  //     })
-
-  //     setCourses(allCourses.data.Items)
-  //   }
-
-  //   getAllCourses()
-  // }, [])
-
   useEffect(() => {
-    setCourses([
-      {
-        name: 'Course 1',
-        holes: 9,
-        par: 27,
-        yards: 1100,
-        holesData: [
-          {
-            hole: 1,
-            yards: 101,
-            par: 3
-          },
-          {
-            hole: 2,
-            yards: 102,
-            par: 3
-          },
-          {
-            hole: 3,
-            yards: 103,
-            par: 3
-          },
-          {
-            hole: 4,
-            yards: 104,
-            par: 3
-          },
-          {
-            hole: 5,
-            yards: 105,
-            par: 3
-          },
-          {
-            hole: 6,
-            yards: 106,
-            par: 3
-          },
-          {
-            hole: 7,
-            yards: 107,
-            par: 3
-          },
-          {
-            hole: 8,
-            yards: 108,
-            par: 3
-          },
-          {
-            hole: 9,
-            yards: 109,
-            par: 3
-          },
-        ]
-      },
-      {
-        name: 'Course 2',
-        holes: 9,
-        par: 27,
-        yards: 1200,
-        holesData: [
-          {
-            hole: 1,
-            yards: 101,
-            par: 3
-          },
-          {
-            hole: 2,
-            yards: 102,
-            par: 3
-          },
-          {
-            hole: 3,
-            yards: 103,
-            par: 3
-          },
-          {
-            hole: 4,
-            yards: 104,
-            par: 3
-          },
-          {
-            hole: 5,
-            yards: 105,
-            par: 3
-          },
-          {
-            hole: 6,
-            yards: 106,
-            par: 3
-          },
-          {
-            hole: 7,
-            yards: 107,
-            par: 3
-          },
-          {
-            hole: 8,
-            yards: 108,
-            par: 3
-          },
-          {
-            hole: 9,
-            yards: 109,
-            par: 3
-          },
-        ]
-      }
-    ])
+    const getAllCourses = async () => {
+      const allCourses = await axios({
+        method: 'POST',
+        url: `${process.env.REACT_APP_API_GATEWAY}/data`,
+        headers: {
+          'x-api-key': `${process.env.REACT_APP_COURSES_API_KEY}`
+        },
+        data: {
+          action: 'GET_ALL',
+          type: 'courses'
+        }
+      })
+
+      setCourses(allCourses.data.Items)
+    }
+
+    getAllCourses()
   }, [])
+
+  // useEffect(() => {
+  //   setCourses([
+  //     {
+  //       name: 'Course 1',
+  //       holes: 9,
+  //       par: 27,
+  //       yards: 1100,
+  //       holesData: [
+  //         {
+  //           hole: 1,
+  //           yards: 101,
+  //           par: 3
+  //         },
+  //         {
+  //           hole: 2,
+  //           yards: 102,
+  //           par: 3
+  //         },
+  //         {
+  //           hole: 3,
+  //           yards: 103,
+  //           par: 3
+  //         },
+  //         {
+  //           hole: 4,
+  //           yards: 104,
+  //           par: 3
+  //         },
+  //         {
+  //           hole: 5,
+  //           yards: 105,
+  //           par: 3
+  //         },
+  //         {
+  //           hole: 6,
+  //           yards: 106,
+  //           par: 3
+  //         },
+  //         {
+  //           hole: 7,
+  //           yards: 107,
+  //           par: 3
+  //         },
+  //         {
+  //           hole: 8,
+  //           yards: 108,
+  //           par: 3
+  //         },
+  //         {
+  //           hole: 9,
+  //           yards: 109,
+  //           par: 3
+  //         },
+  //       ]
+  //     },
+  //     {
+  //       name: 'Course 2',
+  //       holes: 9,
+  //       par: 27,
+  //       yards: 1200,
+  //       holesData: [
+  //         {
+  //           hole: 1,
+  //           yards: 101,
+  //           par: 3
+  //         },
+  //         {
+  //           hole: 2,
+  //           yards: 102,
+  //           par: 3
+  //         },
+  //         {
+  //           hole: 3,
+  //           yards: 103,
+  //           par: 3
+  //         },
+  //         {
+  //           hole: 4,
+  //           yards: 104,
+  //           par: 3
+  //         },
+  //         {
+  //           hole: 5,
+  //           yards: 105,
+  //           par: 3
+  //         },
+  //         {
+  //           hole: 6,
+  //           yards: 106,
+  //           par: 3
+  //         },
+  //         {
+  //           hole: 7,
+  //           yards: 107,
+  //           par: 3
+  //         },
+  //         {
+  //           hole: 8,
+  //           yards: 108,
+  //           par: 3
+  //         },
+  //         {
+  //           hole: 9,
+  //           yards: 109,
+  //           par: 3
+  //         },
+  //       ]
+  //     }
+  //   ])
+  // }, [])
 
   return (
     <>

b85afe5ccc4acb3244be6f3381da213d47e3406f
PatrickGHub <patrick@pkelly.co>
2023-01-03 16:52:09 +1100

    Get rounds from DynamoDB

--- src/routes/Rounds.tsx
+++ src/routes/Rounds.tsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
+import axios from 'axios'
 
 const Rounds = () => {
   const [searchParams] = useSearchParams()
   const course = searchParams.get('course')
+  const [rounds, setRounds] = useState([])
+
+  useEffect(() => {
+    const getRounds = async () => {
+      const allRounds = await axios({
+        method: 'POST',
+        url: `${process.env.REACT_APP_API_GATEWAY}/data`,
+        headers: {
+          'x-api-key': `${process.env.REACT_APP_COURSES_API_KEY}`
+        },
+        data: {
+          action: 'GET_ALL',
+          type: 'rounds'
+        }
+      })
+
+      setRounds(allRounds.data.Items)
+    }
+
+    getRounds()
+  }, [])
 
   return (
     <div>
